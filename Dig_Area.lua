5d04804be6560c5a298bb4cba54e53fa3febd8e9
ScamJogo <raphaelbird5@gmail.com>
2026-01-21 18:26:40 -0600

    dig

---
+++ Dig_Area.lua
@@ -0,0 +1,142 @@
+print("How wide do you want the area?")
+Area = tonumber(read())
+
+print("How deep do you want the area")
+
+Depth = tonumber(read())
+
+Local_X = 0 
+Local_Y = 0
+Local_Z = 0 
+
+Direction = "+X"
+Dig = true
+
+function DigUpDown()
+    if Dig == true then
+        if turtle.detect() then
+            turtle.dig()
+        end
+
+        if turtle.detectUp() then
+            turtle.digUp()
+        end
+
+        if turtle.detectDown() then
+            turtle.digDown()
+        end
+    end
+end
+
+function Move_Forward()
+    if Direction =="+X" then
+        Local_X = Local_X + 1
+        DigUpDown()
+        turtle.forward()
+        DigUpDown()
+    end
+
+    if Direction =="-X" then
+        Local_X = Local_X - 1 
+        DigUpDown()
+        turtle.forward()
+        DigUpDown()
+    end
+
+    if Direction =="+Z" then
+        Local_Z = Local_Z + 1 
+        DigUpDown()
+        turtle.forward()
+        DigUpDown()
+    end
+
+    if Direction =="-Z" then
+        Local_Z = Local_Z - 1 
+        DigUpDown()
+        turtle.forward()
+        DigUpDown()
+    end
+end
+
+function Move_Up()
+    Local_Y = Local_Y + 1
+    turtle.up()
+end
+
+function Move_Down()
+    Local_Y = Local_Y - 1
+    turtle.down()
+end
+
+function TurnRight()
+    if Direction == "+X" then
+        Direction = "+Z"
+        turtle.turnRight()
+    end
+
+    if Direction == "+Z" then
+        Direction = "-X"
+        turtle.turnRight()
+    end
+
+    if Direction == "-X" then
+        Direction = "-Z"
+        turtle.turnRight()
+    end
+
+    if Direction == "-Z" then
+        Direction = "+X"
+        turtle.turnRight()
+    end
+end
+
+function TurnLeft()
+    if Direction == "+X" then
+        Direction = "-Z"
+        turtle.turnLeft()
+    end
+
+    if Direction == "-Z" then
+        Direction = "-X"
+        turtle.turnLeft()
+    end
+
+    if Direction == "-X" then
+        Direction = "+Z"
+        turtle.turnLeft()
+    end
+
+    if Direction == "+Z" then
+        Direction = "+X"
+        turtle.turnLeft()
+    end
+end
+
+function Spiral()
+    TurnLeft()
+    while Local_Z > Area / 2 do
+        Move_Forward()
+    end
+
+    TurnRight()
+
+    while Local_X < Area  do
+        Move_Forward()
+    end
+
+    TurnRight()
+
+    while Local_Z < Area do
+        Move_Forward()
+    end
+
+    TurnRight()
+
+    while Local_X > Area /2 do
+        Move_Forward()
+    end
+end
+
+function DigArea()
+    
+end
