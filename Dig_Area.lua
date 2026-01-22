89493f58be9743379d7c164aab29ff6f27750250
ScamJogo <raphaelbird5@gmail.com>
2026-01-21 19:49:55 -0600

    5555555

--- Dig_Area.lua
+++ Dig_Area.lua
@@ -105,7 +105,7 @@ end
 function Spiral()
     TurnLeft() -- Face -Z
     print("step 1") 
-   while Local_Z > -Area / 2 -1 do 
+   while Local_Z > -Area / 2 +1 do 
         Move_Forward()
     end
 
@@ -117,19 +117,19 @@ function Spiral()
 
     TurnRight() -- Face +Z
     print("step 3")
-    while Local_Z < Area +1 do
+    while Local_Z < Area -1 do
         Move_Forward()
     end
 
     TurnRight() -- Face -X
     print("step 4")
-    while Local_X > -Area -2 do
+    while Local_X > -Area +2 do
         Move_Forward()
     end
 
     TurnRight() -- Face -Z
     print("step 5")
-    while Local_Z > -Area / 2 -1 do
+    while Local_Z > -Area / 2 +1 do
         Move_Forward()
     end
 
